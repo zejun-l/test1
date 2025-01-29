f28657a72d0f0db9e2cca4a501604681d7e05a41
zejun-l <zejunli@umich.edu>
2025-01-29 09:10:15 -0500

    Updated testcode.py

--- testcode.py
+++ testcode.py
@@ -1,2 +1,5 @@
-# testing
-# commit 1
+# git instructions: 
+# first use cd to change directory to where you want the repo -- SI206
+# next use git clone to clone repo
+# next use cd to change diretory to the repo you're working with -- test1
+# now you can actually push stuff correctly 
